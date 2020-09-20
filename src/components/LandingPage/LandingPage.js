7b94bd5f9d65634f913017e739ca071c52031ad8
AlecTech <eremeevoleg@gmail.com>
2020-09-19 21:15:05 -0600

    Added jsx to the Landing page, main elements

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,10 +1,41 @@
 import React from "react";
+import styles from "../LandingPage/LandingPage.module.css";
 
 const LandingPage = () => {
   return (
-    <>
-      <h2>This is Home Page</h2>
-    </>
+  <section className="timer-container">
+    <section className="timer">
+      <div>
+        <span className="mdi mdi-calendar-clock timer-icon"></span>
+        <h2>CountDown Timer</h2>
+        <p>Coupon expire date</p>
+      </div>
+      <div>
+        <section>
+            <p>00</p>
+            <p><small>Days</small></p>
+        </section>
+        <span>:</span>
+        <section>
+            <p>00</p>
+            <p><small>Hours</small></p>
+        </section>
+        <span>:</span>
+        <section>
+            <p>00</p>
+            <p><small>Min</small></p>
+        </section>
+        <span>:</span>
+        <section>
+            <p>00</p>
+            <p><small>Sec</small></p>
+        </section>
+      </div>
+    </section>
+    <section>
+      <button id="btn"> Get Promotion Code </button>
+    </section>
+  </section>
   );
 };
 
