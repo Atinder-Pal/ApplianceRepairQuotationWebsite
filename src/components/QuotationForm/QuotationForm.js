80e2ac55e9fa73d2ea0dad7f91c419db66b89b4b
Atinder-Pal <atty.pal46@gmail.com>
2020-09-20 13:38:11 -0600

    added function to print Quotation Form in pdf

--- src/components/QuotationForm/QuotationForm.js
+++ src/components/QuotationForm/QuotationForm.js
@@ -1,12 +1,29 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import jsPDF from "jspdf";
+import html2canvas from 'html2canvas';
 
 
 const QuotationForm = () => {
   const quoteInfo = useSelector(state => state.repair);
+  
+  // Print the Quotation Form in PDF
+  // Citation
+  //https://stackoverflow.com/questions/44989119/generating-a-pdf-file-from-react-components
+  const printDocument =(e) => {
+    e.preventDefault();
+    const input = document.getElementById('downloadable-form');
+    html2canvas(input).then((canvas) => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF();
+        pdf.addImage(imgData, 'JPEG', 0, 0);
+        // pdf.output('dataurlnewwindow');
+        pdf.save("download.pdf");
+      });
+  //End Citation
+  }
   return (
-    <>
+    <div id='downloadable-form'>
       <h2>QuotationForm</h2>
         <p>Customer Name: <span>{ quoteInfo.newQuotation.name }</span></p>
         <p>Customer Email: <span>{ quoteInfo.newQuotation.email }</span></p>
@@ -14,11 +31,8 @@ const QuotationForm = () => {
         <p>Brand: <span> {quoteInfo.newQuotation.brand }</span></p>
         <p>Issue: <span> { quoteInfo.newQuotation.issue }</span></p>
         <p>Quote: $ <span> { quoteInfo.newQuotation.price }</span></p>  
-        <button type='button'>Download PDF</button><br />
-
-
-      
-    </>
+        <button type='button' onClick={printDocument}>Download PDF</button><br />      
+    </div>
   );
 };
 
