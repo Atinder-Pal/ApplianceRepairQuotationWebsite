78855aab06b7a9fc371a70fea58e12692e39ab0f
Atinder-Pal <atty.pal46@gmail.com>
2020-09-20 13:19:18 -0600

    added basic data pulling in QuotationForm.js

--- src/components/QuotationForm/QuotationForm.js
+++ src/components/QuotationForm/QuotationForm.js
@@ -2,10 +2,22 @@ import React from "react";
 import { useSelector } from "react-redux";
 import jsPDF from "jspdf";
 
+
 const QuotationForm = () => {
+  const quoteInfo = useSelector(state => state.repair);
   return (
     <>
       <h2>QuotationForm</h2>
+        <p>Customer Name: <span>{ quoteInfo.newQuotation.name }</span></p>
+        <p>Customer Email: <span>{ quoteInfo.newQuotation.email }</span></p>
+        <p>Appliance: <span> { quoteInfo.newQuotation.appliance }</span></p>
+        <p>Brand: <span> {quoteInfo.newQuotation.brand }</span></p>
+        <p>Issue: <span> { quoteInfo.newQuotation.issue }</span></p>
+        <p>Quote: $ <span> { quoteInfo.newQuotation.price }</span></p>  
+        <button type='button'>Download PDF</button><br />
+
+
+      
     </>
   );
 };
