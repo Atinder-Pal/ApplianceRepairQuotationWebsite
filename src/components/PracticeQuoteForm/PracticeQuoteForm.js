e2546bc466c67c89d061f3f1122722ee8ff4face
Atinder-Pal <atty.pal46@gmail.com>
2020-09-22 14:09:54 -0600

    tried basic styling using mdbreact

--- src/components/PracticeQuoteForm/PracticeQuoteForm.js
+++ src/components/PracticeQuoteForm/PracticeQuoteForm.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import jsPDF from "jspdf";
 import html2canvas from 'html2canvas';
 import styles from "./QuotationForm.module.css";
-import { MDBCard, MDBCardTitle, MDBCardText, MDBContainer,  MDBBtn, MDBIcon } from "mdbreact";
+
 
 const QuotationForm = () => {
   const quoteInfo = useSelector(state => state.repair);
@@ -25,16 +25,9 @@ const QuotationForm = () => {
   }
   return ( 
     <>
-    <MDBContainer>
-      <MDBCard className="card-body" style={{ marginTop: "5rem", marginBottom: "5rem" }}>     
-        
-        <div className="d-flex justify-content-center">
-          <img src="" alt="Company Logo" className="p-2 col-example text-left" />
-          <MDBCardTitle className="p-2 col-example text-left"> Appliance Repair Quote <MDBIcon icon="file-invoice-dollar" /></MDBCardTitle>
-        </div>
-        <MDBCardText>
-        <div id='downloadableForm' className={styles.quotationForm}>
-          {/* <h2 className={styles.quoteHeading}>Quotation</h2> */}
+
+      <div id='downloadableForm' className={styles.quotationForm}>
+          <h2 className={styles.quoteHeading}>Quotation</h2>
           <p>Company Name: <span>ApplianceMaster.Ltd</span> </p>
           <p>Customer Name: <span>{ quoteInfo.newQuotation.name }</span></p>
           <p>Customer Email: <span>{ quoteInfo.newQuotation.email }</span></p>
@@ -43,14 +36,7 @@ const QuotationForm = () => {
           <p>Issue: <span> { quoteInfo.newQuotation.issue }</span></p>
           <p>Quote: <span> ${ quoteInfo.newQuotation.price }</span></p>       
       </div>     
-        </MDBCardText>
-        <MDBBtn gradient="aqua" className={styles.pdfButton} onClick={printDocument}>Download PDF</MDBBtn>
-        
-      </MDBCard>
-    </MDBContainer>
-
-      
-        
+      <button type='button' className={styles.pdfButton} onClick={printDocument}>Download PDF</button><br />    
     </>
   );
 };
