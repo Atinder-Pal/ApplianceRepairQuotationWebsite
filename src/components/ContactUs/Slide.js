a134cad1474dd6f8614a30ecfb33d2dd07375074
Alex8711 <liuzhendell@gmail.com>
2020-09-24 20:37:43 -0600

    remove inline style; fix warning on the navbar

--- src/components/ContactUs/Slide.js
+++ src/components/ContactUs/Slide.js
@@ -1,35 +1,23 @@
-import React from 'react';
-import { Slide } from 'react-slideshow-image';
-import './Slide.css';
-import 'react-slideshow-image/dist/styles.css';
-import img1 from './Images/img1.jpg';
-import img2 from './Images/img2.jpg';
-import img3 from './Images/img3.jpg';
-import img4 from './Images/img4.jpg';
+import React from "react";
+import { Slide } from "react-slideshow-image";
+import "./Slide.css";
+import "react-slideshow-image/dist/styles.css";
 
 const Slideshow = () => {
   return (
     <div className="slide-container">
       <Slide>
         <div className="each-slide">
-          <div style={{ background: `url(${img1})` }}>
-            <span>Slide1</span> 
-          </div>
+          <div className="showImg1"></div>
         </div>
         <div className="each-slide">
-          <div style={{ background: `url(${img2})` }}>
-            <span>Slide2</span>
-          </div>
+          <div className="showImg2"></div>
         </div>
         <div className="each-slide">
-          <div style={{ background: `url(${img3})` }}>
-            <span>Slide3</span>
-          </div>
+          <div className="showImg3"></div>
         </div>
         <div className="each-slide">
-          <div style={{ background: `url(${img4})` }}>
-            <span>Slide4</span>
-          </div>
+          <div className="showImg4"></div>
         </div>
       </Slide>
     </div>
