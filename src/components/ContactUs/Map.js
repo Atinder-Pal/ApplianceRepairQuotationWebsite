3788d01155771aa1841445fd1586e5c204976b2c
AlecTech <eremeevoleg@gmail.com>
2020-09-25 15:43:07 -0600

    Added image for the ContactUs form desk1, fixed issue with unmounted render, added pins to the googlemap and display message

--- src/components/ContactUs/Map.js
+++ src/components/ContactUs/Map.js
@@ -1,5 +1,5 @@
-import React from "react";
-import {GoogleMap, withScriptjs, withGoogleMap, Marker} from "react-google-maps";
+import React, { useState } from "react";
+import {GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow} from "react-google-maps";
 import styles from '../ContactUs/Map.module.css';
 
 //========= Fake JSON call data loacation Start ==========//
@@ -7,11 +7,11 @@ function getJSONMarkers()
 {
     const markers = 
     [
-      {name:  "AM Calgary Office", location: [51.120751, -114.010572]},
-      {name: "AM Edmonton Office", location: [53.535411, -113.507996]},
-      {name: "AM Moscow Office", location: [55.7504461, 37.6174943]},
-      {name: "AM RedDeer Office", location: [52.269838, -113.818359]},
-      {name: "AM Banff Office", location: [51.1777781, -115.5682504]}
+      {name:  "AM Calgary Office", location: [51.120751, -114.010572], phone: "+1(403)973-8894"},
+      {name: "AM Edmonton Office", location: [53.535411, -113.507996], phone: "+1(780)555-5555"},
+      {name: "AM Moscow Office", location: [55.7504461, 37.6174943], phone: "+7(916)777-7777"},
+      {name: "AM RedDeer Office", location: [52.269838, -113.818359], phone: "+1(560)444-4444"},
+      {name: "AM Banff Office", location: [51.1777781, -115.5682504], phone: "+1(403)333-3333"}
     ];
     return markers;
   }
@@ -19,21 +19,48 @@ function getJSONMarkers()
 //========== Face JSON call Data ends =========//
 
 function Maap(){
+    // Hook
+    const [selectedOffice, setSelectedOffice] = useState(null);
+
     // return map which starts at clagary with 5 pins to show office locations
     return(
         <GoogleMap 
-        defaultZoom={10} 
-        defaultCenter={{lat: 51.047310, lng: -114.057968}}
+            defaultZoom={10} 
+            defaultCenter={{lat: 51.047310, lng: -114.057968}}
         >
-        {getJSONMarkers().map((office) => (
-            <Marker 
-            key={office.name}
-            position={{
-                lat:office.location[0],
-                lng:office.location[1],
-            }}
-            />
-        ))}
+            {getJSONMarkers().map((office) => (
+                <Marker 
+                    key={office.name}
+                    position={{
+                        lat:office.location[0],
+                        lng:office.location[1],
+                    }}
+                    // add Event for display info message on pins. when selected marker set.
+                    onClick={() => {
+                        setSelectedOffice(office);
+                    }}
+                />
+            ))}
+            {selectedOffice && (
+                <InfoWindow
+                    position={{
+                        lat: selectedOffice.location[0],
+                        lng: selectedOffice.location[1],
+                    }}
+                    onCloseClick={() => {
+                        setSelectedOffice(null);
+                    }}
+                >
+                        <div>
+                            <h2>
+                                {selectedOffice.name}
+                            </h2>
+                            <h3>
+                                {selectedOffice.phone}
+                            </h3>
+                        </div>
+                </InfoWindow>
+            )}
         </GoogleMap>
     );
 }
