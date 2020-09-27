0019aab5f8f10f6a07549dcbc517e166beaba655
Erpreet <preetkaurchd15@gmail.com>
2020-09-27 01:37:28 -0600

    Comments Added, Phone Validation Added, Unnecessary Code Removed, Form Validation css Changed, Form Look Changed @ Contact Us

--- src/components/ContactUs/Form.js
+++ src/components/ContactUs/Form.js
@@ -6,6 +6,8 @@ import emailjs from "emailjs-com";
 import apiKeys from "./apikeys";
 import { MDBBtn } from "mdbreact";
 
+// create a form by taking components with state management and  display the errors, if any
+
 const Form = () => {
   const handleContactUsSubmit = (values) => {
     // e.preventDefault();
@@ -19,24 +21,38 @@ const Form = () => {
     );
     console.log("Submitted Successfully");
   };
+
+  // calling handleContactUsSubmit in useForm function here, passing submit actually and it will do same exact behavior (the constants declared and can be reused wherever required)
+  // customized per function using custom hook
   const { handleChange, handleSubmit, values, errors } = useForm(
     handleContactUsSubmit,
     validate
   );
 
   return (
+    // form component 
+    // label // input for name
+    //label // input for phone
+    //label //input for email
+    //label // input for message
+    // submit button
+
     <div className="centered">
-      <form onSubmit={handleSubmit} noValidate>
-        {<h1>Contact Us</h1>}
+      <form onSubmit={handleSubmit} noValidate> 
+
+      {/* no validate for browser not to display validation messages */}
+        <h1>Shoot Message</h1>
 
         <div>
           <label> Name </label>
-          <div>
+          <div >
             <input
-              className={`errros.name && "inputError"}`}
+            //conditional rendering (if and error message)
+              className={`${errors.name && "inputError"}`}
               name="name"
               type="text"
-              value={values.name}
+              //values are being passed inside of these inputs, functions are being passed onChange and handleSubmit function being passed onto Form
+              value={values.name} 
               onChange={handleChange}
             />
             {errors.name && <p className="error">{errors.name}</p>}
@@ -47,7 +63,7 @@ const Form = () => {
           <label> Phone </label>
           <div>
             <input
-              className={`errros.phone && "inputError"}`}
+              className={`${errors.phone && "inputError"}`}
               name="phone"
               type="number"
               value={values.phone}
@@ -61,7 +77,7 @@ const Form = () => {
           <label> Email </label>
           <div>
             <input
-              className={`errros.email && "inputError"}`}
+              className={`${errors.email && "inputError"}`}
               name="email"
               type="email"
               value={values.email}
@@ -74,17 +90,19 @@ const Form = () => {
         <div>
           <label> Message </label>
           <div>
-            <textarea
-              name="message"
-              type="message"
+            <textarea 
+              name="message"  
+              type="text"
               value={values.message}
               onChange={handleChange}
+              //  rows={20}
+              // columns={20}
             />
           </div>
         </div>
 
         {/* <button type="submit">Submit </button> */}
-        <div className="text-center">
+        <div>
           <MDBBtn gradient="aqua" type="submit">
             {" "}
             Submit
