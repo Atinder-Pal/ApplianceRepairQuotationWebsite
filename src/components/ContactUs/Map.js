f7fd85bed1673299e2c8e2629f06c08cef6c1339
AlecTech <eremeevoleg@gmail.com>
2020-10-01 10:29:12 -0600

    Removed Google API key

--- src/components/ContactUs/Map.js
+++ src/components/ContactUs/Map.js
@@ -74,7 +74,7 @@ export default function Map() {
     <div className={styles.backGround}>
         <div className={styles.borderMap} style={{width:'100vm', height: '100vh'}}>
             <WrappedMap 
-            googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAzznb-EUNNuCWG0DVOqJekL0LOHSHp4CE'} 
+            googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'} 
             
             loadingElement={<div style={{height: "100%"}}/>}
             containerElement={<div style={{height: "100%"}}/>}
